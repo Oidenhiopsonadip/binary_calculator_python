d6212b75384e790c88eb0af5c7a3896844fc9c42
Henry Moore <henrydmoore23@gmail.com>
2021-09-27 21:24:12 -0700

    message

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from os import listdir
+import os
+import msvcrt
 
-files = dict(zip(range(len(listdir('.'))), (file for file in os.listdir('.') if file.endswith('.py') if file != 'main.py'))
+files = dict(zip(range(len(os.listdir('.'))), (x for x in os.listdir('.') if x.endswith('.py') if x != 'main.py')))
+for x in files.values():
+    
+
 
 if os.name == 'posix':
     clear = 'clear'
 elif os.name == 'nt':
     clear = 'cls'
-    
 
-""" if __name__ == '__main__':
+
+if __name__ == '__main__':
     cont = False
     while True:
         if cont == True and input(' Input 1 to Continue Running Program:') != '1':
@@ -16,4 +20,3 @@ elif os.name == 'nt':
         cont = True
         os.system(clear)
         print(' 1. Default Calculator             2. Fibonacci Sequence')
-        commandRouter[input(' : ')]() """
